14cd6a442c41344d38d392e2e62c6d98d7b64ad8
denis balazic <denis.balazic@nobula.eu>
2022-11-01 12:40:58 +0100

    Show authentication and role in navigation bar

--- example-app/src/components/Login.tsx
+++ example-app/src/components/Login.tsx
@@ -3,7 +3,7 @@ import usePseudoStore from "../pseudoStore/usePseudoStore";
 import {useNavigate} from "react-router-dom";
 
 const Login = () => {
-    const [role, setRole] = useState<string>();
+    const [role, setRole] = useState<string | undefined>();
     const {setUser} = usePseudoStore();
     const navigate = useNavigate();
 
@@ -15,8 +15,10 @@ const Login = () => {
     return (
         <form onSubmit={handleSubmit}>
             <select value={role} onChange={(e) => setRole(e.target.value)}>
+                <option value={undefined}>no role</option>
                 <option value="user">user</option>
                 <option value="admin">admin</option>
+                <option value="superadmin">superadmin</option>
             </select>
             <button>Submit</button>
         </form>
--- example-app/src/components/Navigation.tsx
+++ example-app/src/components/Navigation.tsx
@@ -3,7 +3,8 @@ import {NavLink} from "react-router-dom";
 import usePseudoStore from "../pseudoStore/usePseudoStore";
 
 const Navigation = () => {
-    const {setUser} = usePseudoStore();
+    const {getUser, setUser} = usePseudoStore();
+    const {authed, roles} = getUser();
     const handleLogout = () => {
         setUser(false, undefined);
     }
@@ -22,10 +23,23 @@ const Navigation = () => {
             <NavLink to={"/cats"} end className={({isActive}) => isActive ? 'navlink active' : 'navlink'}>
                 Cats
             </NavLink>
-            <NavLink to={"/login"} end className={({isActive}) => isActive ? 'navlink active' : 'navlink'}>
-                Login
-            </NavLink>
-            <button className="navlink" onClick={handleLogout}>Logout</button>
+
+            {authed ? (
+                <>
+                    <div className="navlink" style={{display: "flex"}}>
+                        <p>Authed:</p>
+                        {roles ? roles.map((role: string) => (
+                            <p>{role}</p>
+                        )) : 'no role'}
+                    </div>
+                    <div className="navlink" onClick={handleLogout}>Logout</div>
+                </>
+            ) : (
+                <NavLink to={"/login"} end className={({isActive}) => isActive ? 'navlink active' : 'navlink'}>
+                    Login
+                </NavLink>
+            )}
+
         </div>
     );
 };
